6643d34a46589da07ac6c9bee2f7129803c490dd
sergio152001 <sergio.rrojas@mail.escuelaing.edu.co>
2024-05-20 19:14:01 -0500

    nombre de la base de datos datos_sensor.db

---
+++ src/src/build/CMakeFiles/SensorDataApp.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/tmp/Trabajo_Iot/src/src/base_datos.cpp" "CMakeFiles/SensorDataApp.dir/base_datos.cpp.o" "gcc" "CMakeFiles/SensorDataApp.dir/base_datos.cpp.o.d"
+  "/tmp/Trabajo_Iot/src/src/main_consulta.cpp" "CMakeFiles/SensorDataApp.dir/main_consulta.cpp.o" "gcc" "CMakeFiles/SensorDataApp.dir/main_consulta.cpp.o.d"
+  "/tmp/Trabajo_Iot/src/src/sensores.cpp" "CMakeFiles/SensorDataApp.dir/sensores.cpp.o" "gcc" "CMakeFiles/SensorDataApp.dir/sensores.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/src/build/CMakeFiles/SensorDataApp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SensorDataApp.dir/base_datos.cpp.o"
+  "CMakeFiles/SensorDataApp.dir/base_datos.cpp.o.d"
+  "CMakeFiles/SensorDataApp.dir/main_consulta.cpp.o"
+  "CMakeFiles/SensorDataApp.dir/main_consulta.cpp.o.d"
+  "CMakeFiles/SensorDataApp.dir/sensores.cpp.o"
+  "CMakeFiles/SensorDataApp.dir/sensores.cpp.o.d"
+  "SensorDataApp"
+  "SensorDataApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SensorDataApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
